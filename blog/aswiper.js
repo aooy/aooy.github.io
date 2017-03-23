1b4d80983b355887dda824ef568f576b35bdc83d
yangjingzhuo <yangjingzhuo@jiuan.com>
2017-03-23 22:22:06 +0800

    swiper demo

---
+++ blog/aswiper.js
@@ -0,0 +1,16 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(factory((global.myswiper = global.myswiper || {})));
+}(this, (function (exports) { 
+'use strict';
+
+
+function myswiper(){
+	var option = {};
+}
+
+exports.a = {};
+
+//Object.defineProperty(exports, '__esModule', { value: true });
+})));
